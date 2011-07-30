d34cf0a944d2ed3bfa6e5a47b19d78f6a0e26e08
Sebastian Serrano <sebastian@devsar.com>
2011-07-30 19:58:21 -0300

    filter data by membership

--- djProject/apps/api/resources.py
+++ djProject/apps/api/resources.py
@@ -12,9 +12,18 @@ class ProjectResource(ModelResource):
         resource_name = 'project'
         authorization = Authorization()
 
+    def get_object_list(self, request):
+        
+        projects = super(ProjectResource, self).get_object_list(request)        
+        return projects.filter(member__user=request.user)
+
 
 class TaskResource(ModelResource):
     class Meta:
         queryset = Task.objects.all()
         resource_name = 'task'
         authorization = Authorization()
+
+    def get_object_list(self, request):        
+        tasks = super(TaskResource, self).get_object_list(request)        
+        return tasks.filter(project__member__user=request.user)
--- djProject/apps/projects/models.py
+++ djProject/apps/projects/models.py
@@ -1,7 +1,10 @@
+import logging
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 
+import pusher
 
 PROJECT_STATUS = (
     ('active', _("Active")),
@@ -21,6 +24,21 @@ class Project(models.Model):
     since = models.DateTimeField(auto_now_add=True)
     status = models.CharField(choices=PROJECT_STATUS, max_length=12)
     feed = models.URLField(max_length=512, null=True, blank=True)
+    
+    def save(self, *args , ** kwargs ):
+        super(Project, self).save(* args, ** kwargs)
+        try:
+            pusher.app_id = '6417'
+            pusher.key = '93d0509ce62dd993b05a'
+            pusher.secret = 'c824cb5eb3b9c96f863b'           
+            
+            p = pusher.Pusher()
+            for user in User.objects.all():
+                p[user.username].trigger('new_project', {
+                    'project': self.to_dict()
+                })
+        except:
+            logging.exception("error notifying")
 
 class Member(models.Model):
     user = models.ForeignKey(User)

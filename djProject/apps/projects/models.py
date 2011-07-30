64a52731c8acdc29a034ba6649749e20dc812019
pancho <pancho@pancho-laptop.(none)>
2011-07-30 18:37:50 -0300

    create project

--- djProject/apps/projects/models.py
+++ djProject/apps/projects/models.py
@@ -1,3 +1,6 @@
+import datetime 
+from dateutil.relativedelta import *
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
@@ -21,9 +24,40 @@ class Project(models.Model):
     since = models.DateTimeField(auto_now_add=True)
     status = models.CharField(choices=PROJECT_STATUS, max_length=12)
     feed = models.URLField(max_length=512)
+    
+    #TODO: transaction here
+    def new(self, creator, name):
+        """
+            create a new projects, this add membership and add default sprints  
+        """
+        from sprints.models import Sprint
+        self.name = name
+        self.creator = creator
+        self.status = 'active'
+        #TODO: add feed url
+        self.save()
+          
+        member = Member()
+        member.new(creator, self, 'leader')
+        
+        sprint = Sprint()
+        start_date = datetime.datetime.now()
+        end_date = start_date + relativedelta(weeks=1)
+        sprint.new(self, 'Sprint', start_date, end_date)
+        
+        return self
+        
 
 class Member(models.Model):
     user = models.ForeignKey(User)
     project = models.ForeignKey(Project)
     role = models.CharField(choices=USER_ROLES, max_length=12)
 
+    def new(self, user, project, role):
+        self.user = user
+        self.project = project
+        self.role = role
+        self.save()
+        
+        
+        

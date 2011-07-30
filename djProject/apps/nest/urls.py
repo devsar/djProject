e6592889e25a0afeeeec1346c3b430a799a13969
Diego Paez <diego@devsar.com>
2011-07-30 18:48:33 -0300

    add login view

--- djProject/apps/nest/urls.py
+++ djProject/apps/nest/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'nest.views.home', name='nest_home'),
     url(r'^signup$', 'nest.views.signup', name='nest_signup'),
-   
+    url(r'^login$', 'nest.views.log_in', name='nest_login'),
+    url(r'^logout$', 'nest.views.log_out', name="nest_logout"),
 )
 
 
--- djProject/apps/nest/views.py
+++ djProject/apps/nest/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404, redirect
@@ -14,9 +14,7 @@ def home(request):
     return TemplateResponse(request, 'index.html', {})
 
 def signup(request):
-
     form = SignUpForm()
-    
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -29,19 +27,24 @@ def signup(request):
     }
     return TemplateResponse(request, 'nest/signup.html', data)
 
-def login(request):
+def log_in(request):
     form = LoginForm()
     if request.method == 'POST':
         form = LoginForm(request.POST)
-        username = form['username']
-        password = form['password']
-        if form.is_valid():
-            user = authenticate(username=username, password=password)
-            if user.is_active:
-                login(request, user)
-                return redirect('nest_home')
-    
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        logging.error(user)
+        if user and user.is_active:
+            login(request,user)
+            return redirect('nest_home')
+
     data = {
         'form':form,    
     }
     return TemplateResponse(request, 'nest/login.html', data)
+
+def log_out(request):
+    logout(request)
+    return redirect('nest_home')
+    

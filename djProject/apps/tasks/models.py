79eacf2772697b67f38632ce49f335dd8408dfb2
Diego Paez <diego@devsar.com>
2011-07-30 12:15:19 -0300

    add tasks models

--- djProject/apps/tasks/models.py
+++ djProject/apps/tasks/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from sprints.models import *
+
+STATUS_CHOICES = (
+    ('O','Open'),
+    ('C','Closed'),
+    ('P', 'In Progress'),
+    ('B','Blocked'),
+)
+
+PRIORITY_CHOICES = (
+    ('1','High'),
+    ('2','Medium'),
+    ('3','Low')
+    
+)
+
+class Task:
+    label = models.CharField(max_length=255)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES)
+    owner = models.ForeignKey(User)
+    spend = models.FloatField(default=0)
+    parent = models.ForeignKey('self')
+    priority = models.CharField(max_length=2, choices=PRIORITY_CHOICES)
+    sprint = models.ForeignKey(Sprint, null=True, blank=True)
+    
+class Tag:
+    label = models.CharField(max_length=255)
+    task = models.ForeignKey(Task)
+    
+class Log:
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    task = models.ForeignKey(Task)
+    description = models.CharField(max_length=255)
+    
+    
+    

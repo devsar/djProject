40bb5e1dfc7bcaa7d7210300e385b9cd0b9e2cbd
Sebastian Serrano <sebastian@devsar.com>
2011-07-30 12:09:21 -0300

    projects & sprint models

--- djProject/apps/projects/models.py
+++ djProject/apps/projects/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+
+PROJECT_STATUS = (
+    ('active', _("Active")),
+    ('closed', _("Finish")),
+    ('archived', _("Archived")),
+)
+
+USER_ROLES = (
+    ('leader', _("Leader")),
+    ('developer', _("Developer")),
+    ('client', _("Client")),
+)
+
+class Project(models.Model):
+    creator = models.ForeignKey(User)
+    name = models.CharField(max_length=32)
+    since = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=PROJECT_STATUS)
+    feed = models.URLField(max_length=512)
+
+class Member(models.Model):
+    user = models.ForeignKey(User)
+    project = models.ForeignKey(Project)
+    role = models.CharField(choices=USER_ROLES)
+
--- djProject/apps/sprints/models.py
+++ djProject/apps/sprints/models.py
@@ -1,3 +1,27 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import ugettext as _
+
+from projects.models import Project
+
+SPRINT_STATUS = (
+    ('active', _("Active")),
+    ('archived', _("Archived")),
+)
+
+
+class Sprint(models.Model):
+    project = models.ForeignKey(Project)
+    name = models.CharField(max_length=32)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    status = models.CharField(choice=SPRINT_STATUS)
+
+class TimeLog(models.Model):
+    sprint = models.ForeignKey(Sprint)
+    start_time = models.DateTimeField()
+    hours = models.DecimalField()
+    description = models.TextField()
+
+    
+    
